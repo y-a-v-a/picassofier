ad043f6030b503b47f9b218213bfe9e40cc1eb49
Vincent Bruijn <vebruijn@gmail.com>
2018-12-02 21:35:25 +0100

    asyncified

--- picassofier.js
+++ picassofier.js
@@ -3,30 +3,63 @@ const cv = require('opencv');
 const glob = require("glob");
 const assert = require('assert');
 
-// retrieve mask images
-const masks = glob.sync('./masks/mask*.png');
-assert(masks.length > 0, 'Cannot find any masks*.png');
+/**
+ * @returns {Array<string>} List of found mask images
+ */
+function readMasksDir() {
+  const masks = glob.sync('./masks/mask*.png');
+  assert(masks.length > 0, 'Cannot find any masks*.png');
+  return masks;
+}
 
-const masksList = [];
+/**
+ * @returns {Array<string>} List of found jpg files
+ */
+function readSourceDir() {
+  const sourceImages = glob.sync('./input/*.jpg');
+  assert(sourceImages.length > 0, 'Cannot find input images');
+  return sourceImages;
+}
 
-// retrieve person group images
-const sourceImages = glob.sync('./input/*.jpg');
-assert(sourceImages.length > 0, 'Cannot find input images');
+/**
+ * @param {Array<string>} masksList List of files to open
+ * @returns {Promise<Promise<gd.Image>>}
+ */
+async function loadMasks(masksList) {
+  // create Promises to load masks
+  const loadingPromises = masksList.map(loadMask);
 
-const sourceImagesList = [];
+  return await Promise.all(loadingPromises);
+}
 
-// create Promises to load masks
-masks.forEach(el => {
-  masksList.push(new Promise((resolve, reject) => {
-    resolve(gd.openPng(el));
-  }));
-});
+/**
+ * Load path to mask file as png using node-gd
+ * @param {stirng} pathName
+ * @returns {Promise<gd.Image>}
+ */
+async function loadMask(pathName) {
+  return await new Promise((resolve, reject) => {
+    resolve(gd.openPng(pathName));
+  }).catch(error => {
+    throw new Error(error);
+  });
+}
 
-// create promises to load destination images
-sourceImages.forEach(el => {
-  sourceImagesList.push(new Promise((resolve, reject) => {
-    // open input image and make it 1024 wide proportionally
-    gd.openJpeg(el, (error, image) => {
+/**
+ * Read path to masks and load them as pngs
+ * @returns {Promise<gd.Image>}
+ */
+async function getMasksAsPng() {
+  const masksDirList = readMasksDir();
+  return await loadMasks(masksDirList);
+}
+
+/**
+ * open input image and make it 1024 wide proportionally
+ */
+async function scaleJpeg(pathToJpeg) {
+  return await new Promise((resolve, reject) => {
+    gd.openJpeg(pathToJpeg, (error, image) => {
       if (error) {
         reject(error);
       }
@@ -44,62 +77,130 @@ sourceImages.forEach(el => {
         resolve(newImage);
       });
     });
-  }))
-})
-
-Promise.all(masksList).then(masks => {
-  Promise.all(sourceImagesList).then(mainImages => {
-    // all Promises resolved, loop over destination images
-    mainImages.forEach((mainImage, idx) => {
-      // load jpeg pointer into opencv
-      cv.readImage(mainImage.jpegPtr(100, true), function(error, opencvMatrix) {
-        if (error || !opencvMatrix) {
-          throw error;
-        }
-        // start face detection
-        opencvMatrix.detectObject(cv.FACE_CASCADE, {
-          scale: 1.3,
-          neighbors: 3,
-          min: [80,80]
-        }, function(error, facesData) {
-          if (error || !facesData) {
-            console.log(`No facesData for ${mainImage}`);
-          }
-          // for every detected face, apply a mask to it
-          for (let i = 0; i < facesData.length; i++) {
-            // grab random mask
-            let idx = Math.round(Math.random() * (masks.length - 1));
-            const randomMask = masks[idx];
-            const faceCoords = facesData[i];
-
-            // reposition mask relative to detected face
-            const offsetX = 50;
-            const offsetY = 90;
-
-            // copy mask upon face
-            randomMask.copyResized(mainImage,
-              faceCoords.x - offsetX, faceCoords.y - offsetY,
-              0, 0,
-              faceCoords.width + (offsetX * 2), faceCoords.height + (offsetY * 2),
-              randomMask.width, randomMask.height
-            );
-            // debug: show detected face as rectangle
-            // mainImage.rectangle(faceCoords.x, faceCoords.y, faceCoords.x + faceCoords.width, faceCoords.y + faceCoords.height, 0xff0000);
-          }
-
-          // save resulting image
-          mainImage.saveJpeg(`./output/output-${Date.now()}.jpg`, Math.round(Math.random() * 10), (error) => {
-            if (error) {
-              throw error;
-            }
-          });
-        });
-      });
-    });
-  }).catch(function(error) {
-    console.log(error);
   });
-}).catch(function(error) {
-  console.log(error);
-});
+}
+
+/**
+ * @returns {string} New filePath
+ */
+function getNewFileName() {
+  return `./output/output-${Date.now()}.jpg`;
+}
+
+/**
+ * @returns {number} A random rounded number between 0 and 10
+ */
+function getRandomJpegQuality() {
+  return Math.round(Math.random() * 10);
+}
+
+/**
+ * Save image
+ * @param {string} pathName Path to the location where to store the image
+ * @param {gd.Image} imageData Data to store in the file
+ */
+async function saveImage(pathName, imageData) {
+  const quality = getRandomJpegQuality();
+  return await new Promise((resolve, reject) => {
+    imageData.saveJpeg(pathName, quality, error => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(true);
+    });
+  });
+}
+
+/**
+ * Get JPEG image data as raw data
+ * @param {gd.Image} gdImage
+ * @returns {Buffer}
+ */
+function getJpegData(gdImage) {
+  return gdImage.jpegPtr(100, true);
+}
+
+/**
+ * Gets the open cv matrix of jpeg data
+ * @param {Buffer} jpegData
+ * @retusn {Promise}
+ */
+async function getOpencvMatrix(jpegData) {
+  return new Promise((resolve, reject) => {
+    // load jpeg pointer into opencv
+    cv.readImage(jpegData, (error, opencvMatrix) => {
+      if (error || !opencvMatrix) {
+        return reject(error || 'No opencvMatrix');
+      }
+      resolve(opencvMatrix);
+    });
+  });
+}
+
+/**
+ * @param {Object}
+ * @returns {Promise<Array>}
+ */
+async function getFacesData(opencvMatrix) {
+  const detectionOptions = {
+    scale: 1.3,
+    neighbors: 3,
+    min: [80,80]
+  };
+
+  return await new Promise((resolve, reject) => {
+    opencvMatrix.detectObject(cv.FACE_CASCADE, detectionOptions, async (error, facesData) => {
+      if (error || !facesData) {
+        return reject(error || `No facesData found`);
+      }
+      resolve(facesData);
+    });
+  });
+}
+
+async function main() {
+  const maskImages = await getMasksAsPng();
+  const sourceImages = readSourceDir();
+
+  const sourceImagesResampled = await Promise.all(sourceImages.map(async element => {
+    return await scaleJpeg(element);
+  }));
+
+  sourceImagesResampled.forEach(async mainImage => {
+    const jpegData = getJpegData(mainImage);
+    const opencvMatrix = await getOpencvMatrix(jpegData);
+
+    // start face detection
+    const facesData = await getFacesData(opencvMatrix);
+
+    // for every detected face, apply a mask to it
+    for (let i = 0; i < facesData.length; i++) {
+      // grab random mask
+      let idx = Math.round(Math.random() * (maskImages.length - 1));
+      const randomMask = maskImages[idx];
+      const faceCoords = facesData[i];
+
+      // reposition mask relative to detected face
+      const offsetX = 50;
+      const offsetY = 90;
+
+      // copy mask upon face
+      randomMask.copyResized(mainImage,
+        faceCoords.x - offsetX, faceCoords.y - offsetY,
+        0, 0,
+        faceCoords.width + (offsetX * 2), faceCoords.height + (offsetY * 2),
+        randomMask.width, randomMask.height
+      );
+      // debug: show detected face as rectangle
+      // mainImage.rectangle(faceCoords.x, faceCoords.y, faceCoords.x + faceCoords.width,
+      // faceCoords.y + faceCoords.height, 0xff0000);
+    }
+
+    // save resulting image
+    const fileName = getNewFileName();
+    const saved = await saveImage(fileName, mainImage);
+  });
+}
+
+main();
 
